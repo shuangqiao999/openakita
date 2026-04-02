2a4f3b3a5982b671b02b882b1b40c4059604c058
akita <zacon365@gmail.com>
2026-04-03 00:12:47 +0800

    fix: MdModules remarkGfm/rehypeRaw 类型改为 any 兼容 Pluggable

--- apps/setup-center/src/views/chat/utils/chatTypes.ts
+++ apps/setup-center/src/views/chat/utils/chatTypes.ts
@@ -48,8 +48,8 @@ export type MdModules = {
   ReactMarkdown: typeof import("react-markdown").default;
   remarkPlugins: import("react-markdown").Options["remarkPlugins"];
   rehypePlugins: import("react-markdown").Options["rehypePlugins"];
-  remarkGfm?: unknown;
-  rehypeRaw?: unknown;
+  remarkGfm?: any;
+  rehypeRaw?: any;
 };
 
 /** Message queued for sequential sending */
